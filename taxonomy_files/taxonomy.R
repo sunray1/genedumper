2bb083af221bbd886fe63a1a3e8dae613e156148
Chandra Earl <sunray1@bellsouth.net>
2019-07-09 21:50:29 -0400

    Changed script so input can be all Species, but only checks genus

--- taxonomy_files/taxonomy.R
+++ taxonomy_files/taxonomy.R
@@ -1,15 +1,17 @@
 library(taxize)
-Sys.setenv(ENTREZ_KEY="2ab4a57550b4801d78fa9ac204b373348308")
-csvin <- read.csv(file = "genus_list.csv", header=FALSE)
+library(stringr)
+Sys.setenv(ENTREZ_KEY="ENTER_ENTREZ_KEY_HERE")
+csvin <- read.csv(file = "Species_list.csv", header=FALSE)
 csvlist <- levels(csvin$V1)
-#csv_first <- csvlist[1:10]
-#csv_second <- csvlist[423:843]
-class_test <- tax_name(query = csvlist, get = c("Kingdom", "Phylum", "Class", "Order", 'Family'), db = "ncbi")
-#class_test <- classification(csv_first, db="ncbi")
-write.csv(class_test[,2:7], file = "Taxonomy.csv", row.names = FALSE, quote=FALSE)
+csvlist_split <- str_split_fixed(csvlist, " ", 2)
+genera <- unique(csvlist_split[,1])
 
-#class_test <- classification(csvlist, db="gbif")
-#for (i in class_test){
-#  write(str_c(i[1], collapse = ","), file = "taxonomy.csv", append=TRUE)
-#}
+class_test <- tax_name(query = genera, get = c("Kingdom", "Phylum", "Class", "Order", 'Family'), db = "ncbi")
+colnames(csvlist_split) <- c("Genus", "Epithet")
+taxa_merge <- merge(csvlist_split, class_test, by.x="Genus", by.y="query")
+taxise_out <- cbind(taxa_merge[,3:ncol(class_test)],class_test[,2])
+final_tax <- cbind(taxa_merge[,4:ncol(taxa_merge)],taxa_merge[,1], paste(taxa_merge[,1], taxa_merge[,2], sep = " "))
+colnames(final_tax) <- c("Kingdom", "Phylum", "Class", "Order", 'Family', 'Genus', 'Species')
+final_tax[is.na(final_tax)] <- ""
 
+write.csv(final_tax, file = "Taxonomy.csv", row.names = FALSE, quote=FALSE)
