d12d868869660f3198391e1b491fdecb51f6eb28
Chandra Earl <sunray1@ufl.edu>
2019-12-09 13:53:25 -0500

    Update taxonomy.R

--- taxonomy_files/taxonomy.R
+++ taxonomy_files/taxonomy.R
@@ -10,7 +10,7 @@ genera <- unique(csvlist_split[,1])
 class_test <- tax_name(query = genera, get = c("Kingdom", "Phylum", "Class", "Order", 'Family'), db = "ncbi")
 colnames(csvlist_split) <- c("Genus", "Epithet")
 taxa_merge <- merge(csvlist_split, class_test, by.x="Genus", by.y="query")
-taxise_out <- cbind(taxa_merge[,3:ncol(class_test)],class_test[,2])
+#taxise_out <- cbind(taxa_merge[,3:ncol(class_test)],class_test[,2])
 final_tax <- cbind(taxa_merge[,4:ncol(taxa_merge)],taxa_merge[,1], paste(taxa_merge[,1], taxa_merge[,2], sep = " "))
 colnames(final_tax) <- c("Kingdom", "Phylum", "Class", "Order", 'Family', 'Genus', 'Species')
 final_tax[is.na(final_tax)] <- ""
