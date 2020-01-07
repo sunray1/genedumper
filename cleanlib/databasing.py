46979f8b7911d82ff7b0e6ce9405158a98c2713a
Chandra Earl <sunray1@ufl.edu>
2020-01-07 18:23:27 -0500

    fix c error

--- cleanlib/databasing.py
+++ cleanlib/databasing.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 #script designed to create and execute local blast for cleaning
 
-def get_seqs_from_sqldb_GI(GIset, seqtype, blastdb):
+def get_seqs_from_sqldb_GI(GIset, seqtype, blastdb, gene, c):
 	#returns iterator of seqrecs
     #seqtype = qseq or hseq
     import os, sys, sqlite3
     from Bio.SeqRecord import SeqRecord
     from Bio.Seq import Seq
-    conn = sqlite3.connect(blastdb)
-    c = conn.cursor()
     GIset = str(GIset).replace("[", "(").replace("]", ")")
     if seqtype == "hseq":
-        for iter in c.execute("SELECT accession, "+ seqtype +" FROM blast WHERE GI IN "+ GIset +";"):
+	sql = "SELECT accession, "+ seqtype +" FROM blast WHERE Gene_name = '" + gene + "' AND GI IN "+ GIset +";"
+        for iter in c.execute(sql):
             record = SeqRecord(Seq(str(iter[1])), id = iter[0],  description = "")
             yield(record)
     if seqtype == "qseq":
@@ -19,22 +18,18 @@ def get_seqs_from_sqldb_GI(GIset, seqtype, blastdb):
             record = SeqRecord(Seq(str(iter[1])), id = iter[0],  description = "")
             yield(record)
 
-    conn.close()
 
 
-def get_seqs_from_sqldb(accset, seqtype, blastdb):
+def get_seqs_from_sqldb(accset, seqtype, blastdb, c):
 	#returns iterator of seqrecs
     #seqtype = qseq or hseq
     import os, sys, sqlite3
     from Bio.SeqRecord import SeqRecord
     from Bio.Seq import Seq
-    conn = sqlite3.connect(blastdb)
-    c = conn.cursor()
     accset = str(list(accset)).replace("{", "(").replace("}", ")").replace("[", "(").replace("]", ")")
     for iter in c.execute("SELECT accession, "+ seqtype +" FROM blast WHERE accession IN "+ accset +";"):
         record = SeqRecord(Seq(str(iter[1])), id = iter[0],  description = "")
         yield(record)
-    conn.close()
 #usage
 #seqs = get_seqs_from_sqldb(["1080121667", "914615309"], "hseq", "blast_results.db")
 #print(next(seqs))
