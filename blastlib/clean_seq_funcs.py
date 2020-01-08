cb299957453b3525cacccb49486fac18807b5f4b
Chandra Earl <sunray1@ufl.edu>
2020-01-07 22:24:51 -0500

    local_blast

--- blastlib/clean_seq_funcs.py
+++ blastlib/clean_seq_funcs.py
@@ -119,102 +119,88 @@ def alignment_comp(align_GIs, blastdb, qseqbool, c):
     align = AlignIO.read(StringIO(stdout), "clustal")
     return(align)
 
-def blast_all(blast_list, blast_nums, blast_tcids, c, email, taxdb):
-    Entrez.email = email
-    ent_query = get_blast_query(taxdb)
+def blast_all(blast_list, blast_nums, blast_tcids, c, gene, taxdb, blastdb):
     hit_levels_return = []
-    joined_GIs = ",".join(blast_list)
-    error = True
-    while error == True:
-        try:
-            handle = Entrez.efetch(db="nucleotide", rettype="fasta", retmode="text", id=joined_GIs)
-            error = False
-        except:
-            print("Error, trying again")
-            time.sleep(10)
-    seqs =  SeqIO.parse(handle, "fasta")
-    handle_string = StringIO()
-    SeqIO.write(seqs, handle_string, "fasta")
-    data = handle_string.getvalue()
-    error = True
-    while error == True:
-        try:
-            result_handle = NCBIWWW.qblast("blastn", "nt", data, entrez_query=ent_query, word_size=28, hitlist_size=100)
-            error = False
-        except:
-            print("Error, trying again")
-            time.sleep(10)
-    blast_records = NCBIXML.parse(result_handle)
-    #this iterates in a wierd way - note next at the end - this is due to the structure of ncbi's blast returns
-    for rec in blast_records:
-        #get GI - blast doesnt pull down GI nums anymore
-        queryacc = str(rec.query.split()[0])
-        for iter in c.execute("SELECT GI FROM blast WHERE accession = '" + queryacc + "'"):
-            rec_GI = str(iter[0])
-        #get taxonomy for tc_id
-        rank = ''
-        taxonomy = []
-        sp_tc_id = blast_tcids[rec_GI]
-        while rank != 'Family':
-            for iter in c.execute("SELECT r.namestr, tc.parent_id FROM taxon_concepts tc, ranks r WHERE tc_id = '" + str(sp_tc_id) + "' AND tc.rank_id = r.rank_id"):
-                rank = iter[0]
-                sp_tc_id = iter[1]
-            for iter in c.execute("SELECT n.namestr FROM names n, names_to_taxonconcepts ntt WHERE ntt.name_id = n.name_id AND tc_id = '" + str(sp_tc_id) + "'"):
-                taxonomy.append(str(iter[0]))
-        hit_levels_all = []
-        for i in range(blast_nums[rec_GI]):
-            query_acc = rec.query.split()[0]
-#            print(query_acc)
-            hitdic = {}
-            hitSp = []
-            warning_counts = 0
-            for alignment in rec.alignments:
-                for hsp in alignment.hsps:
-                    identity=float(hsp.identities)/float(hsp.align_length)
-                if alignment.title.split("|")[3] == query_acc:
-                    pass
+    iterator = get_seqs_from_sqldb_GI(blast_list, "hseq", blastdb, gene, c)
+    export_fasta(iterator, "error_seqs.fa")
+    local_blast(gene+"_db.fa", "error_seqs.fa")
+    with open("error_seqs.fa.xml") as p:
+        print("Parsing blast output")
+        blast_records = NCBIXML.parse(p)
+        #this iterates in a wierd way - note next at the end - this is due to the structure of ncbi's blast returns
+        for rec in blast_records:
+            #get GI - blast doesnt pull down GI nums anymore
+            queryacc = str(rec.query.split()[0])
+            for iter in c.execute("SELECT GI FROM blast WHERE accession = '" + queryacc + "'"):
+                rec_GI = str(iter[0])
+            #get taxonomy for tc_id
+            rank = ''
+            taxonomy = []
+            sp_tc_id = blast_tcids[rec_GI]
+            while rank != 'Family':
+                for iter in c.execute("SELECT r.namestr, tc.parent_id FROM taxon_concepts tc, ranks r WHERE tc_id = '" + str(sp_tc_id) + "' AND tc.rank_id = r.rank_id"):
+                    rank = iter[0]
+                    sp_tc_id = iter[1]
+                for iter in c.execute("SELECT n.namestr FROM names n, names_to_taxonconcepts ntt WHERE ntt.name_id = n.name_id AND tc_id = '" + str(sp_tc_id) + "'"):
+                    taxonomy.append(str(iter[0]))
+            hit_levels_all = []
+            for i in range(blast_nums[rec_GI]):
+                query_acc = rec.query.split()[0]
+    #            print(query_acc)
+                hitdic = {}
+                hitSp = []
+                warning_counts = 0
+                for alignment in rec.alignments:
+                    for hsp in alignment.hsps:
+                        identity=float(hsp.identities)/float(hsp.align_length)
+                    if alignment.hit_def == query_acc:
+                        pass
+                    else:
+                        for iter in c.execute("SELECT GI FROM blast WHERE accession='" + alignment.hit_def + "'"):
+                            hitGI = (str(iter[0]))
+                        hitdic[str(hitGI)] = identity
+                #hitdic is GI: identity of all hits for a query
+                if len(hitdic.values()) > 5:
+                    maxiden = sorted(hitdic.values(), reverse = True)[0:5]
                 else:
-                    hitdic[str(alignment.title.split("|")[1])] = identity
-            if len(hitdic.values()) > 5:
-                maxiden = sorted(hitdic.values(), reverse = True)[0:5]
-            else:
-                maxiden = hitdic.values()
-            try:
-                hitGIs = [GI for GI, iden in hitdic.iteritems() if iden in maxiden] #python2
-            except:
-                hitGIs = [GI for GI, iden in hitdic.items() if iden in maxiden] #python3
-            for hitGI in hitGIs:
-                prev_len = len(hitSp)
-    #get species for all the top hits for each GI
-                for iter in c.execute("SELECT Species FROM blast WHERE GI='" + hitGI + "'"):
-                    hitSp.append(str(iter[0]))
-                if len(hitSp) == prev_len:
-                    warning_counts += 1
-#                    print("Warning: " + str(hitGI) + " is a top hit, but is not in the blast database")
-            hit_levels = []
-            if len(hitSp) > 0:
-                if warning_counts < 2:
-                    for hit_species in hitSp:
-                        hit = -1
-                        for iter in c.execute("SELECT ntt.tc_id FROM names_to_taxonconcepts ntt, names n WHERE n.name_id = ntt.name_id AND namestr='" + hit_species + "'"):
-                            sp_tc_id = iter[0]
-                        while hit == -1:
-                            for iter in c.execute("SELECT r.namestr, tc.parent_id, tc.tc_id FROM taxon_concepts tc, ranks r WHERE tc_id = '" + str(sp_tc_id) + "'AND tc.rank_id = r.rank_id"):
-                                sp_tc_id = iter[1]
-                            for iter in c.execute("SELECT n.namestr FROM names n, names_to_taxonconcepts ntt WHERE ntt.name_id = n.name_id AND tc_id = '" + str(sp_tc_id) + "'"):
-                                if str(iter[0]) in taxonomy:
-                                    hit = taxonomy.index(iter[0])
-                                    hit_levels.append(hit)
-                    hit_levels_all.append(min(hit_levels))
+                    maxiden = hitdic.values()
+                try:
+                    hitGIs = [GI for GI, iden in hitdic.iteritems() if iden in maxiden] #python2
+                except:
+                    hitGIs = [GI for GI, iden in hitdic.items() if iden in maxiden] #python3
+                #hitGIs - GIs with the highest identity
+                for hitGI in hitGIs:
+                    prev_len = len(hitSp)
+        #get species for all the top hits for each GI - hitsp
+                    for iter in c.execute("SELECT Species FROM blast WHERE GI='" + hitGI + "'"):
+                        hitSp.append(str(iter[0]))
+                    if len(hitSp) == prev_len:
+                        warning_counts += 1
+                        #print("Warning: " + str(hitGI) + " is a top hit, but is not in the blast database")
+                hit_levels = []
+                if len(hitSp) > 0:
+                    if warning_counts < 2:
+                        for hit_species in hitSp:
+                            hit = -1
+                            for iter in c.execute("SELECT ntt.tc_id FROM names_to_taxonconcepts ntt, names n WHERE n.name_id = ntt.name_id AND namestr='" + hit_species + "'"):
+                                sp_tc_id = iter[0]
+                            while hit == -1:
+                                for iter in c.execute("SELECT r.namestr, tc.parent_id, tc.tc_id FROM taxon_concepts tc, ranks r WHERE tc_id = '" + str(sp_tc_id) + "'AND tc.rank_id = r.rank_id"):
+                                    sp_tc_id = iter[1]
+                                for iter in c.execute("SELECT n.namestr FROM names n, names_to_taxonconcepts ntt WHERE ntt.name_id = n.name_id AND tc_id = '" + str(sp_tc_id) + "'"):
+                                    if str(iter[0]) in taxonomy:
+                                        hit = taxonomy.index(iter[0])
+                                        hit_levels.append(hit)
+                        hit_levels_all.append(min(hit_levels))
+                    else:
+    #                    print('Warning: GI has two or more hits that aren\'t in the database, will ignore')
+                        hit_levels_all.append(1000)
                 else:
-#                    print('Warning: GI has two or more hits that aren\'t in the database, will ignore')
+    #                print('Warning: no taxonomy info for current GI\'s hits - will look at other possible GI')
                     hit_levels_all.append(1000)
-            else:
-#                print('Warning: no taxonomy info for current GI\'s hits - will look at other possible GI')
-                hit_levels_all.append(1000)
-            if i != blast_nums[rec_GI]-1:
-                rec = next(blast_records)
-        hit_levels_return.append(hit_levels_all)
+                if i != blast_nums[rec_GI]-1:
+                    rec = next(blast_records)
+            hit_levels_return.append(hit_levels_all)
     return(hit_levels_return)
     
     
